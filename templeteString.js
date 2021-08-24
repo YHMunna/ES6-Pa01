3d65b34b25dc23cd3b712a3ac2a05b01adf1e4bf
YHMunna <yeakubhossainmunna2017@gmail.com>
2021-08-25 03:19:14 +0600

    prb

--- templeteString.js
+++ templeteString.js
@@ -1,5 +1,13 @@
+// templeteString by using variable
 let text = "hey ,i love you";
 let name = "munna";
 
 const templeteString = `${text},${name}`;
 console.log(templeteString);
+
+// templeteString by using object
+
+
+let person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+const temObjString = `This is ${person.firstName}`;
+console.log(temObjString);
